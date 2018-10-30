8f4a0f54713a3aededf8fcf1dea80ecbd1e02860
Zach Fuller <zachfuller5280@gmail.com>
2018-10-30 14:30:39 -0400

    Update pLI_functions.R

--- pLI_functions.R
+++ pLI_functions.R
@@ -8,21 +8,5 @@ calc_pli<-function(obs, exp){
   return(init_prob[3])
 }
 
-calc_prec<-function(obs, exp){
-  init_pis<-c(0.2075797,0.4887326,0.3036877)
-  lambdas<-c(1,0.463,0.089)
-  expec_vec<-lambdas*exp
-  pois_vec<-dpois(obs, lambda=expec_vec)
-  init_prob<-(pois_vec * init_pis)/sum((pois_vec * init_pis))
-  return(init_prob[2])
-}
 
-calc_pnull<-function(obs, exp){
-  init_pis<-c(0.2075797,0.4887326,0.3036877)
-  lambdas<-c(1,0.463,0.089)
-  expec_vec<-lambdas*exp
-  pois_vec<-dpois(obs, lambda=expec_vec)
-  init_prob<-(pois_vec * init_pis)/sum((pois_vec * init_pis))
-  return(init_prob[1])
-}
 
